6adbf4455325ac35bc0c805417fbaea029659251
junugi <junugii@gmail.com>
2020-01-20 15:21:34 +1000

    Add confidence as an input for LSTM.  Changes to be committed: 	modified:   YOTMMLP.py

--- YOTMMLP.py
+++ YOTMMLP.py
@@ -11,13 +11,13 @@ class MlpLNet(nn.Module):
         self.seq_len = seq_len
         self.np = np
 
-        self.lstmx = nn.LSTM(input_size=1, hidden_size=self.np.HiddenSize, 
+        self.lstmx = nn.LSTM(input_size=2, hidden_size=self.np.HiddenSize, 
                             num_layers=self.np.LayerSize, dropout=0.3, batch_first=True)
-        self.lstmy = nn.LSTM(input_size=1, hidden_size=self.np.HiddenSize, 
+        self.lstmy = nn.LSTM(input_size=2, hidden_size=self.np.HiddenSize, 
                             num_layers=self.np.LayerSize, dropout=0.3, batch_first=True)
-        self.lstmw = nn.LSTM(input_size=1, hidden_size=self.np.HiddenSize, 
+        self.lstmw = nn.LSTM(input_size=2, hidden_size=self.np.HiddenSize, 
                             num_layers=self.np.LayerSize, dropout=0.3, batch_first=True)
-        self.lstmh = nn.LSTM(input_size=1, hidden_size=self.np.HiddenSize, 
+        self.lstmh = nn.LSTM(input_size=2, hidden_size=self.np.HiddenSize, 
                             num_layers=self.np.LayerSize, dropout=0.3, batch_first=True)
         
         self.init_hidden()
@@ -42,15 +42,23 @@ class MlpLNet(nn.Module):
     def forward(self, x, l):
         batch_size, seq_size, N = l.size()
         l = l.view(batch_size, seq_size, -1)
-        x_out, _ = self.lstmx(l[:,:,0].view(batch_size, seq_size, -1), self.hiddenx)
-        y_out, _ = self.lstmy(l[:,:,1].view(batch_size, seq_size, -1), self.hiddeny)
-        w_out, _ = self.lstmw(l[:,:,2].view(batch_size, seq_size, -1), self.hiddenw)
-        h_out, _ = self.lstmh(l[:,:,3].view(batch_size, seq_size, -1), self.hiddenh)
+        
+        x_out = l[:,:,0].view(batch_size, seq_size, -1)
+        y_out = l[:,:,1].view(batch_size, seq_size, -1)
+        w_out = l[:,:,2].view(batch_size, seq_size, -1)
+        h_out = l[:,:,3].view(batch_size, seq_size, -1)
+        p_out = l[:,:,4].view(batch_size, seq_size, -1)
+
+        x_out, _ = self.lstmx(torch.cat((x_out, p_out), 2), self.hiddenx)
+        y_out, _ = self.lstmy(torch.cat((y_out, p_out), 2), self.hiddeny)
+        w_out, _ = self.lstmw(torch.cat((w_out, p_out), 2), self.hiddenw)
+        h_out, _ = self.lstmh(torch.cat((h_out, p_out), 2), self.hiddenh)
         
         x_out = self.fcx(x_out)
         y_out = self.fcy(y_out)
         w_out = self.fcw(w_out)
         h_out = self.fch(h_out)
+
         c_out = torch.cat((x_out, y_out, w_out, h_out), 2)
         
         return c_out
